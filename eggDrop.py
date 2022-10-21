58088ff2f9e7e1976c21e7d53197c30b697349a6
Nifuramu <kotaro22235963@gmail.com>
2022-10-21 21:18:25 +0900

    fix readme, blackjack, eggdrop

--- eggDrop.py
+++ eggDrop.py
@@ -1,31 +1,63 @@
+#This algorithm is not correct. It outpts wrong answers.
+
 import math
 
-#This algorithm is not correct. It outputs wrong answers. 
-def eggDrop(n,k):
-    if n == 1:
-        return k
-
-    numberOfSearchFloor = k
+def eggDrop(n, k):
     egg = n
+    numberOfFloor = k
 
-    while egg > 2:
-        egg -= 1
-        numberOfSearchFloor = numberOfSearchFloor // 2 
+    if egg == 1:
+        print("patern1")
+        return numberOfFloor
+    elif 2**(egg + 2) > numberOfFloor:
+        print("patern2")
+        return   int(math.log2(numberOfFloor)) + 1
+    
+    print("patern3")#The algorithm of patern3 may be incorrect.
 
-    #print("n-2 : {}".format(n-2))
-    #print("numberOfSearchFloor : {}".format(numberOfSearchFloor))
+    maxColumn = getMaxColumn(numberOfFloor)
+    print(maxColumn)
+    A = [[0 for i in range(maxColumn)] for j in range(egg-1)]
+    
+    nNumber = 0
+    for column in range(maxColumn):
+        nNumber += 1
+        A[0][column] = nNumber*(nNumber+1)/2
 
-    return (n-2)+twoEggDrop(numberOfSearchFloor)    
+    for row in range(egg-1):
+        A[row][0] = 1    
+
+    for i in range(1, egg-1):
+        sumOfRow = 1
+        for column in range(1, maxColumn):
+            sumOfRow += A[i-1][column]
+            
+            A[i][column] = sumOfRow
+            if sumOfRow > k:
+                break
+    print(A)
+            
+    for column in range(maxColumn): 
+        if A[egg-2][column] > numberOfFloor:
+            if egg == 2:
+                return column + 1
+            return column + egg -1
+
+    print(A)      
 
 
-
-def twoEggDrop(numberOfFloor):
+def getMaxColumn(numberOfFloor):
+    print(numberOfFloor)
     n = 1
-    sumOfnum = n*(n+1)/2
-
-    while sumOfnum < numberOfFloor:
+    sumOfNumber = n*(n+1)/2
+    
+    while sumOfNumber < numberOfFloor:
         n += 1
-        sumOfnum = n*(n+1)/2
-
-    #print(n)    
+        sumOfNumber = n*(n+1)/2
+        
     return n
+
+numEgg = input("Please type the number of egg : ")
+numFloor = input("Please type the number of floors : ")
+print(eggDrop(int(numEgg), int(numFloor)))
+
